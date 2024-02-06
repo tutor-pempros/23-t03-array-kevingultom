f364bd884197f5782b2190267769bc1d3aa32208
kevingultom <143678985+kevingultom@users.noreply.github.com>
2024-02-06 21:37:41 +0700

    siap to3 01

--- t03_01.c
+++ t03_01.c
@@ -4,8 +4,32 @@
 
 int main(int _argc, char **_argv)
 {
+  int arr, besar, kecil, i;
+  
+  scanf("%d", &arr);
+int arr1[arr];
+int arr2[arr];
+  for (int i = 0; i < arr; i++)
+  {
+    scanf("%d", &arr1[i]);
+    besar = arr1[i];
+    kecil = arr1[i];
+  }
+  for (int i = 0; i < arr; i++)
+    if (arr1[i] < kecil)
+    {
+      kecil = arr1[i];
+    }
+     if (arr1[i] > besar)
+    {
+      besar = arr1[i];
+    }
 
-  // your code here
+  
+ printf("%d\n", kecil);
+  printf("%d\n", besar);
 
   return 0;
 }
+
+
